700cba497705a8e30cdb8097579a7f855e4c13ea
Vedant Paranjape <22630228+VedantParanjape@users.noreply.github.com>
2020-04-06 02:26:27 +0530

    removed docs/html, will be readded as a submodule

--- docs/html/search/files_0.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['json_5fhandler_2ec',['json_handler.c',['../json__handler_8c.html',1,'']]],
-  ['json_5fhandler_2eh',['json_handler.h',['../json__handler_8h.html',1,'']]]
-];
--- docs/html/search/variables_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['current',['current',['../structpid__terms.html#ab4c7f8712a43d4fe1b8932783f286c22',1,'pid_terms']]]
-];
